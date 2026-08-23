4439dc9286ea17c9bb8cd88a1d96b60c7f02f99c
huseeiin <husseinfadhil651@gmail.com>
2026-08-23 19:54:10 +0300

    a

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 // main.ts
-Deno.serve(async(req: Request) => {
-  const url=new URL(req.url)
+Deno.serve(async(request: Request) => {
+  const url=new URL(request.url)
   const viewkey = url.searchParams.get("id");
   const quality = url.searchParams.get("quality");
 
